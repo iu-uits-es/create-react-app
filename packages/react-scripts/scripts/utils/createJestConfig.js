26bd96d885014b7d73f0a71b1bc4908c5dc71bb6
Andy Hill <athill@iu.edu>
2020-08-27 11:20:10 -0400

    ESIMW-2095: Update to CRA 3.4.1

--- packages/react-scripts/scripts/utils/createJestConfig.js
+++ packages/react-scripts/scripts/utils/createJestConfig.js
@@ -69,7 +69,16 @@ module.exports = (resolve, rootDir, isEjecting) => {
     resetMocks: true,
   };
   if (rootDir) {
-    config.rootDir = rootDir;
+    config.rootDir = paths.appPath;
+    const jestSrcDir = paths.appSrc.replace(config.rootDir, '<rootDir>');
+    const jestTestDir = paths.testSrc.replace(config.rootDir, '<rootDir>');
+    const setupTestsFile = fs.existsSync(paths.testsSetup) ? paths.testsSetup.replace(config.rootDir, '<rootDir>') : undefined;
+    config.testMatch = [
+      `${jestSrcDir}/**/__tests__/**/*.{js,jsx,mjs}`,
+      `${jestSrcDir}/**/?(*.)(spec|test).{js,jsx,mjs}`,
+      `${jestTestDir}/**/?(*.)(spec|test).{js,jsx,mjs}`,
+    ];
+    config.setupTestFrameworkScriptFile = setupTestsFile;
   }
   const overrides = Object.assign({}, require(paths.appPackageJson).jest);
   const supportedKeys = [
---
+++ packages/react-scripts/scripts/watch.js
@@ -0,0 +1,35 @@
+'use strict';
+process.env.NODE_ENV = 'development';
+
+const fs = require('fs-extra');
+const paths = require('../config/paths');
+const webpack = require('webpack');
+const config = require('../config/webpack.config.js')(process.env.NODE_ENV);
+
+config.entry = config.entry.filter(
+  entry => !entry.includes('webpackHotDevClient')
+);
+
+config.output.path = paths.appBuild;
+
+webpack(config).watch({}, (err, stats) => {
+  if (err) {
+    console.error(err);
+  } else {
+    copyPublicFolder();
+  }
+  console.error(
+    stats.toString({
+      chunks: false,
+      colors: true,
+    })
+  );
+});
+
+function copyPublicFolder() {
+  fs.existsSync(paths.appPublic) &&
+    fs.copySync(paths.appPublic, paths.appBuild, {
+      dereference: true,
+      filter: file => file !== paths.appHtml,
+    });
+}
